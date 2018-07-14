efe6027be7ead6625d318ce2752c86fe0d16b151
Caitlyn Fowkes <caitlynfowkes@gmail.com>
2018-07-14 17:13:22 +0200

    Using state within book-detail

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -5,9 +5,14 @@ import { connect } from 'react-redux';
 // dont forget to remove "export default" when exported at the bottom
 class BookDetail extends Component {
     render() {
+        //"this.props.book" is returning null (set in reducer) and cant get title of null so add a check
+        if(!this.props.book){ 
+            return <div>Select a book</div>}
         return (
           <div>
-            Book title
+              <h3>Details for:</h3>
+            <p>{this.props.book.title}</p>
+            <p>Pages: {this.props.book.pages}</p>
           </div>
         )
       }
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,20 @@
 export default function() {
     return [
-        {title: 'Book-1'},
-        {title: 'Book-2'},
-        {title: 'Book-3'},
-        {title: 'Book-4'}
+        {
+        title: 'Book-1',
+        pages: 105
+        },
+        {
+        title: 'Book-2',
+        pages: 1034
+        },
+        {
+        title: 'Book-3',
+        pages: 103
+        },
+        {
+        title: 'Book-4',
+        pages: 107
+        }
     ]
 }
